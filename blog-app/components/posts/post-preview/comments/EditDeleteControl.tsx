7a4325577e95554cba65e48afe784680ff9b6c48
msafty <mohamed.safty@team-parallax.com>
2023-02-28 17:33:15 +0200

    make the post page serverSide isted of static site generation SSG

--- blog-app/components/posts/post-preview/comments/EditDeleteControl.tsx
+++ blog-app/components/posts/post-preview/comments/EditDeleteControl.tsx
@@ -1,18 +1,18 @@
-import { useEffect, useState } from "react";
-import deleteComment from "../../../../utils/delete";
-import Delete from "../../../icons/Delete";
-import Dots from "../../../icons/Dots";
-import Edit from "../../../icons/Edit";
-import LoadingSpinier from "../../../icons/LoadingSpinier";
+import { useEffect, useState } from "react"
+import deleteComment from "../../../../utils/delete"
+import Delete from "../../../icons/Delete"
+import Dots from "../../../icons/Dots"
+import Edit from "../../../icons/Edit"
+import LoadingSpinier from "../../../icons/LoadingSpinier"
 
 interface EditDeleteControlProps {
-	setEdit: React.Dispatch<React.SetStateAction<boolean>>;
-	loading: boolean;
-	setLoading: React.Dispatch<React.SetStateAction<boolean>>;
-	isClicked: boolean;
-	setClick: React.Dispatch<React.SetStateAction<boolean>>;
-	refreshData: () => void;
-	commentId: string;
+	setEdit: React.Dispatch<React.SetStateAction<boolean>>
+	loading: boolean
+	setLoading: React.Dispatch<React.SetStateAction<boolean>>
+	isClicked: boolean
+	setClick: React.Dispatch<React.SetStateAction<boolean>>
+	refreshData: () => void
+	commentId: string
 }
 
 const EditDeleteControl = ({
@@ -24,11 +24,11 @@ const EditDeleteControl = ({
 	refreshData,
 	commentId,
 }: EditDeleteControlProps) => {
-	const [open, setOpen] = useState<boolean>(false);
+	//const [open, setOpen] = useState<boolean>(false);
 
-	useEffect(() => {
-		setOpen(false);
-	}, [commentId]);
+	//useEffect(() => {
+	//	setOpen(false);
+	//}, [commentId]);
 
 	return (
 		<div className="flex gap-1 pt-[3px]">
@@ -39,45 +39,45 @@ const EditDeleteControl = ({
 			>
 				<Dots className="scale-[.80]" />
 			</button>*/}
-					{loading ? (
-						<div className="animate-spin">
-							<LoadingSpinier className=" scale-[.60]" />
-						</div>
-					) : (
-						<button
-							onClick={() => {
-								if (isClicked === false) {
-									deleteComment(commentId);
-									refreshData();
-									setLoading(true);
-									setClick(true);
-								}
-							}}
-							style={{
-								opacity: isClicked ? 0.5 : 1,
-								cursor: isClicked ? "default" : "pointer",
-							}}
-							className="w-fit px-1 py-[5px] bg-gray-100 font-normal rounded-xl bg-opacity-50 duration-150 hover:bg-opacity-80 text-sm"
-						>
-							<Delete className="w-[13px] h-4 sm:w-5" />
-						</button>
-					)}
-					<button
-						onClick={() => {
-							if (isClicked === false) {
-								setEdit((p) => !p);
-							}
-						}}
-						style={{
-							opacity: isClicked ? 0.5 : 1,
-							cursor: isClicked ? "default" : "pointer",
-						}}
-						className="w-fit py-[1px] px-1 sm:px-[5px] sm:py-[5px] bg-gray-100 font-normal rounded-xl bg-opacity-50 duration-150 hover:bg-opacity-80 text-sm"
-					>
-						<Edit className="w-[13px] h-4 sm:w-[18px]" />
-					</button>
+			{loading ? (
+				<div className="animate-spin">
+					<LoadingSpinier className=" scale-[.60]" />
+				</div>
+			) : (
+				<button
+					onClick={() => {
+						if (isClicked === false) {
+							deleteComment(commentId)
+							refreshData()
+							setLoading(true)
+							setClick(true)
+						}
+					}}
+					style={{
+						opacity: isClicked ? 0.5 : 1,
+						cursor: isClicked ? "default" : "pointer",
+					}}
+					className="w-fit px-1 py-[5px] bg-gray-100 font-normal rounded-xl bg-opacity-50 duration-150 hover:bg-opacity-80 text-sm"
+				>
+					<Delete className="w-[13px] h-4 sm:w-5" />
+				</button>
+			)}
+			<button
+				onClick={() => {
+					if (isClicked === false) {
+						setEdit((p) => !p)
+					}
+				}}
+				style={{
+					opacity: isClicked ? 0.5 : 1,
+					cursor: isClicked ? "default" : "pointer",
+				}}
+				className="w-fit py-[1px] px-1 sm:px-[5px] sm:py-[5px] bg-gray-100 font-normal rounded-xl bg-opacity-50 duration-150 hover:bg-opacity-80 text-sm"
+			>
+				<Edit className="w-[13px] h-4 sm:w-[18px]" />
+			</button>
 		</div>
-	);
-};
+	)
+}
 
-export default EditDeleteControl;
+export default EditDeleteControl
