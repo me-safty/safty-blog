e93eb847592a62513bdece125dcb713dabfedaa1
msafty <mohamed.safty@team-parallax.com>
2023-03-04 14:49:10 +0200

    fix aouthors posts

--- blog-app/components/posts/post-preview/index.tsx
+++ blog-app/components/posts/post-preview/index.tsx
@@ -25,7 +25,7 @@ const PostPreview = ({ post }: PostPreviewProps) => {
 	const [postData, setPostData] = useState<Post>(post)
 	const [commentsData, setCommentsData] = useState<comment[]>(post.comments)
 
-	const authorBlogs = postData.author.posts.filter(
+	const authorBlogs = post.author.posts.filter(
 		(blog) => post._id !== blog._id
 	)
 	return (
@@ -43,10 +43,10 @@ const PostPreview = ({ post }: PostPreviewProps) => {
 					/>
 					<div className="lg:w-[56%] relative">
 						<p className=" text-orange-300 font-medium text-lg">
-							{postData.category.title}
+							{post.category.title}
 						</p>
 						<h1 className="text-[26px] lg:text-3xl mb-2 capitalize text-white font-medium leading-10">
-							{postData.title}
+							{post.title}
 						</h1>
 						<div className="flex w-20 h-[5px] gap-1">
 							<div className="w-[80%] group-hover:w-[30%] duration-150 rounded-xl bg-orange-300"></div>
@@ -54,13 +54,13 @@ const PostPreview = ({ post }: PostPreviewProps) => {
 						</div>
 					</div>
 					<div className="relative">
-						<Link href={`/users/${postData.author.slug?.current}`}>
+						<Link href={`/users/${post.author.slug?.current}`}>
 							<div className="flex w-fit items-center gap-3 mt-5 bg-gray-50 bg-opacity-20 p-3 px-4 rounded-xl">
 								<Image
 									src={
-										postData.author.imglink
-											? postData.author.imglink
-											: urlFor(postData.author.image).url()
+										post.author.imglink
+											? post.author.imglink
+											: urlFor(post.author.image).url()
 									}
 									alt="author image"
 									width={48}
@@ -69,10 +69,10 @@ const PostPreview = ({ post }: PostPreviewProps) => {
 								/>
 								<div>
 									<p className="text-white font-medium text-xl">
-										{postData.author.name}
+										{post.author.name}
 									</p>
 									<p className="text-gray-200">
-										{new Date(postData._createdAt).toLocaleString()}
+										{new Date(post._createdAt).toLocaleString()}
 									</p>
 								</div>
 							</div>
@@ -81,26 +81,26 @@ const PostPreview = ({ post }: PostPreviewProps) => {
 							<p className="bg-gray-50 bg-opacity-20 px-3 py-1 rounded-xl">
 								comments:{" "}
 								<span className=" text-fuchsia-500">
-									{postData.comments.length}
+									{post.comments.length}
 								</span>
 							</p>
 						</div>
 					</div>
 				</div>
 				<Image
-					alt={postData.title}
+					alt={post.title}
 					width={900}
 					height={600}
 					className="w-full h-[260px] sm:h-[450px] lg:h-[600px] rounded-3xl object-cover"
-					src={urlFor(postData.mainImage).url()}
+					src={urlFor(post.mainImage).url()}
 				/>
 				<article className="px-3">
 					<div className="mt-10 text-lg px-3">
-						{postData.body ? (
+						{post.body ? (
 							<PortableText
 								dataset={process.env.NEXT_PUBLIC_SANITY_DATASET!}
 								projectId={process.env.NEXT_PUBLIC_PROJECT_ID!}
-								content={postData.body}
+								content={post.body}
 								serializers={{
 									h1: (props: any) => <h1 {...props} />,
 									h2: (props: any) => <h1 {...props} />,
@@ -112,12 +112,12 @@ const PostPreview = ({ post }: PostPreviewProps) => {
 							/>
 						) : (
 							<div>
-								<MarkdownMarkdown source={postData.mdbody} />
+								<MarkdownMarkdown source={post.mdbody} />
 							</div>
 						)}
 					</div>
 				</article>
-				{postData.author.posts.length && (
+				{post.author.posts.length && (
 					<>
 						<hr className=" border border-orange-300 mx-auto max-w-5xl my-10" />
 						<h1 className="text-2xl sm:text-3xl font-semibold px-6 mb-5">
