7016475b100f294761eb275ac92dc0cfcb3ae3eb
msafty <mohamed.safty@team-parallax.com>
2023-03-04 21:56:38 +0200

    remove facebook from the nextauth

--- blog-app/pages/api/auth/[...nextauth].ts
+++ blog-app/pages/api/auth/[...nextauth].ts
@@ -1,7 +1,7 @@
 import NextAuth, { AuthOptions } from "next-auth"
 //import GithubProvider from "next-auth/providers/github"
 import GoogleProvider from "next-auth/providers/google"
-import FacebookProvider from "next-auth/providers/facebook"
+//import FacebookProvider from "next-auth/providers/facebook"
 import { sanityClint } from "../../../utils/sanity"
 import { Author } from "../../../typing"
 
@@ -17,10 +17,10 @@ export const authOptions: AuthOptions = {
 			clientId: process.env.GOOGLE_CLIENT_ID as string,
 			clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
 		}),
-		FacebookProvider({
-			clientId: process.env.FACEBOOK_CLIENT_ID as string,
-			clientSecret: process.env.FACEBOOK_CLIENT_SECRET as string,
-		}),
+		//FacebookProvider({
+		//	clientId: process.env.FACEBOOK_CLIENT_ID as string,
+		//	clientSecret: process.env.FACEBOOK_CLIENT_SECRET as string,
+		//}),
 	],
 	callbacks: {
 		//@ts-ignore
